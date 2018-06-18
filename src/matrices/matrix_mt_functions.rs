0e304bc7ab77be1c5d650ee65e2647a0dde786eb
AuroransSolis <primalucegd@gmail.com>
2018-06-18 12:20:50 -0700

    Files for future additions.

--- src/matrices/matrix_mt_functions.rs
+++ src/matrices/matrix_mt_functions.rs
@@ -1,2 +1,1 @@
-#![cfg(target_feature = "nightly-ext")]
 // Preemptive stuff - will be available in future versions
--- src/matrices/matrix_simd_transforms.rs
+++ src/matrices/matrix_simd_transforms.rs
@@ -1,1 +1,2 @@
 #![cfg(target_feature = "nightly-ext")]
+// Preemptive stuff - will be available in future versions
